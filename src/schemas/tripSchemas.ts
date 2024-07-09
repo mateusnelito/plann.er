a3cda533ff64c6022855416489864bc62dce7a41
Mateus Vale Celestino Nelito <mateusvalecelestinod@gmail.com>
2024-07-09 23:16:48 +0100

    feat: add support to response no body request

--- src/schemas/tripSchemas.ts
+++ src/schemas/tripSchemas.ts
@@ -1,32 +1,35 @@
 import { z } from 'zod';
 import { complexBadRequestSchema } from './badRequestSchema';
 
-const tripSchema = z.object({
-  id: z
-    .string({
-      required_error: 'Id é obrigatório.',
-      invalid_type_error: 'Tipo inválido.',
-    })
-    .uuid({ message: 'UUID inválido.' }),
+const tripSchema = z.object(
+  {
+    id: z
+      .string({
+        required_error: 'Id é obrigatório.',
+        invalid_type_error: 'Tipo inválido.',
+      })
+      .uuid({ message: 'UUID inválido.' }),
 
-  // TODO: add regex validation
-  destination: z
-    .string({
-      required_error: 'destino é obrigatório.',
+    // TODO: add regex validation
+    destination: z
+      .string({
+        required_error: 'destino é obrigatório.',
+        invalid_type_error: 'Tipo inválido.',
+      })
+      .trim()
+      .min(3, { message: 'destino deve possuir mais de 3 caracteres.' })
+      .max(50, { message: 'destino deve possuir no máximo 50 caracteres.' }),
+    startsAt: z.coerce.date({
       invalid_type_error: 'Tipo inválido.',
-    })
-    .trim()
-    .min(3, { message: 'destino deve possuir mais de 3 caracteres.' })
-    .max(50, { message: 'destino deve possuir no máximo 50 caracteres.' }),
-  startsAt: z.coerce.date({
-    invalid_type_error: 'Tipo inválido.',
-    required_error: 'data de inicio é obrigatória.',
-  }),
-  endsAt: z.coerce.date({
-    invalid_type_error: 'Tipo inválido.',
-    required_error: 'data de fim é obrigatória.',
-  }),
-});
+      required_error: 'data de inicio é obrigatória.',
+    }),
+    endsAt: z.coerce.date({
+      invalid_type_error: 'Tipo inválido.',
+      required_error: 'data de fim é obrigatória.',
+    }),
+  },
+  { invalid_type_error: "'O body deve ser um JSON válido.'" }
+);
 
 export const POST_TRIP_SCHEMA = {
   summary: 'Agenda uma nova viagem',
