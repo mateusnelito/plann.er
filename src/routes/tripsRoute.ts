9c999c4aab81c3efd58353364069e1fa5ad86418
Mateus Vale Celestino Nelito <mateusvalecelestinod@gmail.com>
2024-07-09 22:43:30 +0100

    feat: add route to handle post on /api/trips

--- src/routes/tripsRoute.ts
+++ src/routes/tripsRoute.ts
@@ -1,21 +1,13 @@
-import {
-  FastifyInstance,
-  FastifyPluginAsync,
-  FastifyReply,
-  FastifyRequest,
-} from 'fastify';
+import { FastifyInstance, FastifyPluginAsync } from 'fastify';
 import { ZodTypeProvider } from 'fastify-type-provider-zod';
-import HttpStatusCodes from '../utils/HttpStatusCodes';
+import { createTrip } from '../controllers/tripControllers';
+import { POST_TRIP_SCHEMA } from '../schemas/tripSchemas';
 
 const tripsRoute: FastifyPluginAsync = async (server: FastifyInstance) => {
   // Create trip route
   server.withTypeProvider<ZodTypeProvider>().post('/', {
-    // schema: {},
-    handler: (request: FastifyRequest, reply: FastifyReply) => {
-      return reply
-        .status(HttpStatusCodes.CREATED)
-        .send({ message: 'create trips working' });
-    },
+    schema: POST_TRIP_SCHEMA,
+    handler: createTrip,
   });
 };
 
