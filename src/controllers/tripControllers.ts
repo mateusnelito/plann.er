8b53c4438d9e193910e0068af35d362c9fc2c3cd
Mateus Vale Celestino Nelito <mateusvalecelestinod@gmail.com>
2024-07-09 23:49:11 +0100

    feat: add vallidation for date based on business rules

--- src/controllers/tripControllers.ts
+++ src/controllers/tripControllers.ts
@@ -1,12 +1,33 @@
 import { FastifyReply, FastifyRequest } from 'fastify';
 import { postTripType } from '../schemas/tripSchemas';
 import HttpStatusCodes from '../utils/HttpStatusCodes';
+import dayjs from 'dayjs';
+import BadRequest from '../utils/BadRequest';
 
 export async function createTrip(
   request: FastifyRequest<{ Body: postTripType }>,
   reply: FastifyReply
 ) {
   const { destination, startsAt, endsAt } = request.body;
+
+  // Checking dates based on business rules
+  // Check if the begin of trip is in past
+  if (dayjs(startsAt).isBefore(dayjs())) {
+    throw new BadRequest({
+      statusCode: HttpStatusCodes.BAD_REQUEST,
+      message: 'Data de inicio inválida',
+      errors: { startsAt: ['A data de inicio deve ser futura.'] },
+    });
+  }
+
+  // Check if the end of trip is after begin
+  if (dayjs(endsAt).isBefore(startsAt)) {
+    throw new BadRequest({
+      statusCode: HttpStatusCodes.BAD_REQUEST,
+      message: 'Data de fim inválida',
+      errors: { endsAt: ['A data de fim deve ser superior a de inicio.'] },
+    });
+  }
 
   return reply
     .status(HttpStatusCodes.CREATED)
